a190724887bdd866b8889727b097f423678730f5
Frison Alexander <fri.b.alex@gmail.com>
2024-11-13 12:36:03 -0500

    bare minimum express app

--- readme.md
+++ readme.md
@@ -1,2 +1,18 @@
 # Directions
 
+### 1) open powershell and run
+
+```$env:TMDB_API_KEY = "YOUR API KEY HERE"```
+
+### 2) run the application
+
+```cd cvs-api-endpoint```
+
+```npm install```
+
+```npm start```
+
+### 3) make api request
+
+example: 
+```http://localhost:3000/movie?year=2011```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Run express app here")
+import express from 'express';
+
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+if(!TMDB_API_KEY){ throw new Error("TMDB_API_KEY is not set")}
+
+const app = express();
+const PORT = 3000;
+
+app.use('/movie', function(req, res){ res.status(200).json({test: "testing"}) })
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
